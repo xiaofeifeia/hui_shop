602a76e99665714fed3087b12cd1cf7c416c5b2b
xiaofeifeia <626128140@qq.com>
2020-06-13 15:06:02 +0800

    uodate

--- src/main/java/com/xph/shop/dao/UserMapper.java
+++ src/main/java/com/xph/shop/dao/UserMapper.java
@@ -1,9 +1,11 @@
 package com.xph.shop.dao;
-
-import tk.mybatis.mapper.common.BaseMapper;
-
 import com.xph.shop.entity.User;
+import tk.mybatis.mapper.common.Mapper;
 
-public interface UserMapper extends BaseMapper<User>{
-
+/****
+ * @Author:shenkunlin
+ * @Description:User的Dao
+ * @Date 2019/6/14 0:12
+ *****/
+public interface UserMapper extends Mapper<User> {
 }
--- src/main/java/com/xph/shop/entity/User.java
+++ src/main/java/com/xph/shop/entity/User.java
@@ -1,32 +1,64 @@
 package com.xph.shop.entity;
 
-import java.util.Date;
+import javax.persistence.*;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+import java.lang.String;
+import java.lang.Integer;
 
 import lombok.Data;
 
 @Data
-@Table(name="t_user")
-public class User {
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "JDBC")
-	private Long userId;
-	private String userName;
-	private String password;
-	private String email;
-	private String phone;
-	private Byte age;
-	private Byte sex;
-	private String birthDay;
-	/**
-	 * 注册来源
-	 */
-	private Byte registFrom;
-	private Date createTime;
-	private Date updateTime;
+	@Column(name = "user_id")
+	private Integer userId;// 用户id
+
+	@Column(name = "username")
+	private String username;// 用户名
+
+	@Column(name = "nickname")
+	private String nickname;// 别名
+
+	@Column(name = "password")
+	private String password;// 密码
+
+	@Column(name = "source_type")
+	private String sourceType;// 0后台创建 1:微信小程序，2:PC，3：H5，4：Android，5：IOS
+
+	@Column(name = "email")
+	private String email;// 邮箱
+
+	@Column(name = "headPic")
+	private String headPic;// 头像
+
+	@Column(name = "sex")
+	private String sex;// 性别 0 默认0未知 1 男 2 女
+
+	@Column(name = "phone")
+	private String phone;// 手机号码
+
+	@Column(name = "birthday")
+	private String birthday;// 生日
+
+	@Column(name = "status")
+	private String status;// 0 正常 1 禁用 2删除
+
+	@Column(name = "create_time")
+	private Date createTime;// 创建时间
+
+	@Column(name = "update_time")
+	private Date updateTime;// 更新时间
+
+	@Column(name = "last_login_time")
+	private Date lastLoginTime;// 最后登录时间
+
 }
