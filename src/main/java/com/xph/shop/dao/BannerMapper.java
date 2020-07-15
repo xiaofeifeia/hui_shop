fed71b2048876ca40f18fed840965f3903ae622b
xiaofeifeia <626128140@qq.com>
2020-07-15 23:19:08 +0800

    update

---
+++ src/main/java/com/xph/shop/dao/BannerMapper.java
@@ -0,0 +1,10 @@
+package com.xph.shop.dao;
+import com.xph.shop.entity.Banner;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @Author:xph
+ * @Description:Banner的Dao
+ */
+public interface BannerMapper extends Mapper<Banner> {
+}
